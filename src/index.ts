3d3239792e2e834eb185c49a579c4a508960ccf4
Remi <remi.k.work@gmail.com>
2025-12-17 10:02:47 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Effect } from "effect";
+
+const fetchRequest = Effect.tryPromise(() => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"));
+
+const jsonResponse = (response: Response) => Effect.tryPromise(() => response.json());
+
+const savePokemon = (pokemon: unknown) => Effect.tryPromise(() => fetch("/api/pokemon", { body: JSON.stringify(pokemon) }));
+
+const main = fetchRequest.pipe(Effect.flatMap(jsonResponse), Effect.flatMap(savePokemon));
+
+Effect.runPromise(main).then((result) => {
+  console.log(result);
+});

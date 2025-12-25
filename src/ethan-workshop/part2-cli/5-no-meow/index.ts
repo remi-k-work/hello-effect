5103212ced7fda123bda6f988729f82d7498e5dc
Remi <remi.k.work@gmail.com>
2025-12-25 22:13:15 +0100

    5-no-meow

---
+++ src/ethan-workshop/part2-cli/5-no-meow/index.ts
@@ -0,0 +1,73 @@
+import * as fs from "node:fs/promises";
+import { Console, Effect, Exit, Layer, Option } from "effect";
+import { CLIOptions, Fetch, HeaderParseError, TextDecodeError, UnknownError } from "./model.js";
+
+const MainLayer = Layer.mergeAll(CLIOptions.Default, Fetch.Default);
+
+function main() {
+  return Effect.gen(function* () {
+    const { url, method, data, headers, output, include } = yield* CLIOptions;
+
+    const res = yield* getResponse(url, method, headers, data);
+
+    const buffer: string[] = [];
+
+    if (include) {
+      buffer.push(`${res.status} ${res.statusText}`);
+      res.headers.forEach((value, key) => {
+        buffer.push(`${key}: ${value}`);
+      });
+      // Add an empty line to separate headers from body
+      buffer.push("");
+    }
+
+    const text = yield* getText(res);
+    buffer.push(text);
+
+    const finalString = buffer.join("\n");
+    yield* Effect.matchEffect(output, {
+      onSuccess: (output) => Effect.tryPromise(() => fs.writeFile(output, finalString)),
+      onFailure: () => Console.log(finalString),
+    });
+  });
+}
+
+const exit = main().pipe(
+  Effect.provide(MainLayer),
+  Effect.catchTags({
+    TextDecodeError: (error) => Console.error("Text decode error: ", error),
+    UnknownError: (error) => Console.error("Unknown error: ", error),
+  }),
+  Effect.runPromiseExit,
+);
+
+Exit.match(await exit, {
+  onSuccess: () => process.exit(0),
+  onFailure: (cause) => {
+    console.error(cause);
+    process.exit(1);
+  },
+});
+
+// ***
+function getResponse(url: string, method: string, headers: object, data?: string) {
+  return Effect.gen(function* () {
+    const providedFetch = yield* Fetch;
+
+    return yield* Effect.tryPromise({
+      try: (signal) =>
+        providedFetch(url, {
+          method,
+          headers,
+          ...(data && { body: data }),
+          signal,
+        }),
+      catch: (error) => new UnknownError({ error }),
+    });
+  });
+}
+
+function getText(res: Response) {
+  return Effect.tryPromise({ try: () => res.text(), catch: () => new TextDecodeError() });
+}
+// ***
---
+++ src/ethan-workshop/part2-cli/5-no-meow/model.ts
@@ -0,0 +1,36 @@
+import { Data, Effect, Option, pipe, Schema } from "effect";
+import { getCliOption, getCliOptionMultiple } from "./helpers.js";
+import { StringPairsFromStrings } from "./schemas.js";
+
+export class UnknownError extends Data.TaggedError("UnknownError")<{ readonly error: unknown }> {}
+export class TextDecodeError extends Data.TaggedError("TextDecodeError") {}
+export class HeaderParseError extends Data.TaggedError("HeaderParseError") {}
+export class CliOptionsParseError extends Data.TaggedError("CliOptionsParseError")<{ readonly error: unknown }> {}
+
+export class Fetch extends Effect.Service<Fetch>()("Fetch", {
+  succeed: globalThis.fetch,
+}) {}
+
+export class CLIOptions extends Effect.Service<CLIOptions>()("CLIOptions", {
+  effect: Effect.gen(function* () {
+    const args = yield* Effect.sync(() => process.argv);
+
+    const method = getCliOption(args, { name: "method", alias: "X" }).pipe(Option.getOrElse(() => "GET"));
+    const data = getCliOption(args, { name: "data", alias: "d" }).pipe(Option.getOrUndefined);
+    const headers = yield* pipe(
+      getCliOptionMultiple(args, { name: "headers", alias: "H" }),
+      Schema.decode(StringPairsFromStrings),
+      Effect.map((_) => Object.fromEntries(_)),
+      Effect.mapError(() => new HeaderParseError()),
+    );
+    const output = getCliOption(args, { name: "output", alias: "O" });
+    const include = getCliOption(args, { name: "include", alias: "i" }).pipe(
+      Option.flatMap((_) => (["true", "false"].includes(_) ? Option.some(_) : Option.none())),
+      Option.map((_) => _ === "true"),
+    );
+
+    const url = yield* Option.fromNullable(args[2]).pipe(Effect.mapError(() => new CliOptionsParseError({ error: "No url provided" })));
+
+    return { url, method, data, headers, output, include } as const;
+  }),
+}) {}
